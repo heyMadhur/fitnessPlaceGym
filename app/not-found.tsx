4721e33778c0e63f204815f449d018eb72cd3457
heyMadhur <madhurgupta12112002@gmail.com>
2023-07-18 00:29:57 +0530

    notFoundPage

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const notFound = () => {
+  return (
+    <div>
+      This Page is Not Available Brother 404 ERROR
+      <p>fsdfsedafasd</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p>fdsdafsdfasd</p>
+    </div>
+  )
+}
+
+export default notFound;
